33ca8af12a452d164820be8b2a693082d16ea12e
fasunareuu <yt.fasu@bk.ru>
2024-05-03 10:55:52 +0300

    admin panel 1.3.2

--- js/adminPanelLogin.js
+++ js/adminPanelLogin.js
@@ -1,15 +1,42 @@
-function validateLogin() {
+document.querySelector("form").addEventListener('click', (e) => {
   let email = document.getElementById("login").value;
   let password = document.getElementById("password").value;
 
-  // Проверка логина и пароля
-  if(email || password === '') {
-    alert("Пожалуйста заполните все поля!")
-  }
-  if (email === "admin@mail.ru" && password === "admin1234") {
-    alert("Вы успешно вошли в админ панель!");
-    window.location.href = "admin-panel.html";
-  } else {
-    alert("Неправильный логин или пароль!");
-  }
+  const fields = [
+    {
+      id: "login",
+      errorId: "login_error",
+      message: "Логин обязателен для заполнения",
+    },
+    {
+      id: "password",
+      errorId: "password_error",
+      message: "Пароль обязателен для заполнения",
+    }
+  ];
+
+  let isValid = true;
+
+  fields.forEach((field) => {
+    const value = document.querySelector(`#${field.id}`).value;
+    const error = document.querySelector(`#${field.errorId}`);
+
+    if (value === "" || value === null) {
+      e.preventDefault();
+      error.innerHTML = field.message;
+      isValid = false;
+    } else {
+      error.innerHTML = "";
+    }
+  });
+
+  if (isValid) {
+    // Проверка логина и пароля
+    if (email === "admin@mail.ru" && password === "admin1234") {
+        window.location.href = "../html/admin-panel.html";
+    } else {
+        alert("Неправильный логин или пароль!");
+    }
 }
+
+ })
